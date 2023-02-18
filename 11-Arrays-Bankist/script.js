84fbdcda0cdd3ea5ea88067cbb0b7ba829d0a3bf
erenburuk <erenburukk@gmail.com>
2023-02-19 01:13:40 +0300

    Find method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -102,7 +102,6 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
@@ -388,7 +387,6 @@ TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
-*/
 
 // const calcAverageHumanAge = function (ages) {
 //   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
@@ -411,3 +409,14 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(avg1, avg2);
+*/
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
