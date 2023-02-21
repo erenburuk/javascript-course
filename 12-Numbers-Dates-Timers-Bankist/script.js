52a0d251b687d94789d48ccc1ee07cd200718363
erenburuk <erenburukk@gmail.com>
2023-02-21 14:32:58 +0300

    Add BigInt

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -351,7 +351,6 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
-*/
 
 // 287,460,000,000
 const diameter = 287_460_000_000;
@@ -368,3 +367,35 @@ console.log(PI);
 
 console.log(Number('230_000'));
 console.log(parseInt('230_000'));
+*/
+
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(498578932592374827589237253987n);
+console.log(BigInt(498578932));
+
+// Operations
+console.log(10000n + 10000n);
+console.log(726347815781241246897216490124910247128469n * 10000000n);
+// console.log(Math.sqrt(16n));
+
+const huge = 204214245436534634634634n;
+const num = 23;
+console.log(huge * BigInt(num));
+
+// Exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == '20');
+
+console.log(huge + ' is REALLY big!!!');
+
+// Divisions
+console.log(11n / 3n);
+console.log(10 / 3);
