31b298264d3b0c39321f32c0809a3d6c811e6123
erenburuk <erenburukk@gmail.com>
2023-02-24 00:23:03 +0300

    Inheritance - Object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -364,6 +364,7 @@ tesla.brake();
 tesla.accelerate();
 */
 
+/*
 //////////////////////////////////////////////////
 // Inheritance Between "Classes": ES6 Classes
 class PersonCl {
@@ -427,3 +428,33 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+*/
+
+//////////////////////////////////////////////////
+// Inheritance Between "Classes": Object.create
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.calcAge();
