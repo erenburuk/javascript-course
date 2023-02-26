41db31deb85e9fa13c28d5604ec6f28daa22db77
erenburuk <erenburukk@gmail.com>
2023-02-26 19:16:52 +0300

    The module pattern

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -14,6 +14,7 @@ console.log('Importing module');
 // console.log(price);
 
 import add, { cart } from './shoppingCart.js';
+/*
 add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
@@ -41,3 +42,34 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier `);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('apple', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
